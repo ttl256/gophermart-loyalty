6c4b45ef1ca1bd445c02e5f123d8a1b2f5c426ca
ttl256 <64251319+ttl256@users.noreply.github.com>
2026-02-05 11:30:15 +0000

    simplify worker's timing

--- internal/accrual/worker.go
+++ internal/accrual/worker.go
@@ -39,43 +39,41 @@ func NewWorker(repo Repo, client *Client, freq time.Duration) *Worker {
 }
 
 func (w *Worker) Run(ctx context.Context) error { //nolint: gocognit //fine
-	sleep := w.freq
+	ticker := time.NewTicker(w.freq)
+	var retryAt time.Time
 	for {
 		select {
 		case <-ctx.Done():
 			return xerrors.WithStack(ctx.Err())
-		default:
-			select {
-			case <-ctx.Done():
-				return xerrors.WithStack(ctx.Err())
-			case <-time.After(sleep):
-				sleep = w.freq
-				orders, err := w.repo.GetOrdersForProcessing(ctx)
-				if err != nil {
-					w.logger.ErrorContext(ctx, "fetching orders for processing", slog.Any("error", err))
-					return xerrors.WithStack(err)
-				}
-				if len(orders) == 0 {
-					w.logger.DebugContext(ctx, "no orders to process")
-					continue
-				}
-				for _, order := range orders {
-					if err = w.Process(ctx, order.Number); err != nil {
-						var errRateLimit RateLimitError
-						if errors.As(err, &errRateLimit) {
-							w.logger.InfoContext(
-								ctx, "rate limit", slog.Duration("retry_after", errRateLimit.RetryAfter),
-							)
-							sleep = errRateLimit.RetryAfter
-							break
-						}
-						w.logger.ErrorContext(
-							ctx,
-							"processing order",
-							slog.String("order", string(order.Number)),
-							slog.Any("error", fmt.Sprintf("%+v", err)),
+		case <-ticker.C:
+			if time.Now().Before(retryAt) {
+				continue
+			}
+			orders, err := w.repo.GetOrdersForProcessing(ctx)
+			if err != nil {
+				w.logger.ErrorContext(ctx, "fetching orders for processing", slog.Any("error", err))
+				return xerrors.WithStack(err)
+			}
+			if len(orders) == 0 {
+				w.logger.DebugContext(ctx, "no orders to process")
+				continue
+			}
+			for _, order := range orders {
+				if err = w.Process(ctx, order.Number); err != nil {
+					var errRateLimit RateLimitError
+					if errors.As(err, &errRateLimit) {
+						w.logger.InfoContext(
+							ctx, "rate limit", slog.Duration("retry_after", errRateLimit.RetryAfter),
 						)
+						retryAt = time.Now().Add(errRateLimit.RetryAfter)
+						break
 					}
+					w.logger.ErrorContext(
+						ctx,
+						"processing order",
+						slog.String("order", string(order.Number)),
+						slog.Any("error", fmt.Sprintf("%+v", err)),
+					)
 				}
 			}
 		}
